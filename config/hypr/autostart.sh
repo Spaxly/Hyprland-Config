c15add885197722fdf71491439f8bef9767e404c
ohm <ohmrc2545@gmail.com>
2022-09-28 21:40:15 -0400

    vertical bar

--- config/hypr/autostart.sh
+++ config/hypr/autostart.sh
@@ -1,4 +1,5 @@
-swaybg --image ~/.config/hypr/wallpapers/orbit.png &
+swaybg --image ~/.config/hypr/wallpapers/pinksky4k.png &
 mako --config ~/.config/hypr/mako/mocha &
-waybar -c ~/.config/hypr/waybar/config -s ~/.config/hypr/waybar/style.css &
+# waybar -c ~/.config/hypr/waybar/config -s ~/.config/hypr/waybar/style.css &
+waybar -c ~/.config/hypr/waybar/vertical_bar/config -s ~/.config/hypr/waybar/vertical_bar/style.css &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
